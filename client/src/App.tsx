d8ec6ecdf6567d05ee63b2dd1d5d3de94d11d01d
ARTCODE Website <webmaster@my-artcode.com>
2026-09-16 13:19:22 +0000

    Configure Wouter base for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,17 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Router as WouterRouter, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
 
-function Router() {
-  const pageBase = import.meta.env.BASE_URL.replace(/\/$/, "");
-
+function Routes() {
   return (
     <Switch>
-      <Route path={`${pageBase}/`} component={Home} />
-      <Route path={`${pageBase}/404`} component={NotFound} />
+      <Route path="/" component={Home} />
+      <Route path="/404" component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
     </Switch>
@@ -34,7 +32,9 @@ function App() {
       >
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <WouterRouter base={import.meta.env.BASE_URL}>
+            <Routes />
+          </WouterRouter>
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
